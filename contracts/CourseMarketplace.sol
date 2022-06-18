5a4e74452610a8d7a3ef66730c1f3a46a06edbf5
Shoaib7889 <shoaib4030891@gmail.com>
2022-06-18 12:34:33 +0500

    recompiled into public dir"

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -17,15 +17,76 @@ contract CourseMarketplace{
         State state;//1 byte
      }
 
+     //mapping of courseHash to Course data
+     mapping(bytes32 => Course) private ownedCourses;
+     //mapping of courseId to courseHash
+     mapping(uint => bytes32) private ownedCourseHashById;
+
+     uint private totalOwnedCourses ;
+
+     address payable owner;
+
+    /// Course already has an Owner
+    error CourseHasOwner();
+    /// Only owner can call
+    error CallerNotOwner();
+
+     constructor(){
+         //setting for the owner of the contract At Deploy Time
+         setContractOwner(msg.sender);
+     }
+
+     modifier onlyOwner{
+         if(msg.sender != getContractOwner()){
+             revert CallerNotOwner(); 
+         }
+         _;
+     }
+
      //abi and msg are two objects globally available in the Contract
-     function purchaseCourse(bytes16 courseId, bytes32 proof) external payable returns(bytes32) {
-         //course id = 10 ni Ascii text
+     function purchaseCourse(bytes16 courseId, bytes32 proof) external payable { 
+         
+         bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+         if(hasCourseOwnership(courseHash)){
+             revert CourseHasOwner();
+         }
+         uint id = totalOwnedCourses++;
+         ownedCourseHashById[id] = courseHash;
+         ownedCourses[courseHash] = Course({
+             id: id,
+             price: msg.value,
+             proof: proof,
+             owner: msg.sender,
+             state: State.Purchased
+         });
+     }
+     function transferOwnership(address newOwner) external onlyOwner {
+         setContractOwner(newOwner);
+     }
+     function getCourseCount() external view returns(uint) {
+         return totalOwnedCourses;
+     }
+     function getCourseHashByIndex(uint index) external view returns(bytes32) {
+         return ownedCourseHashById[index];
+     }
+     //whenever specify struct, specify memory
+     function getCourseByHash(bytes32 courseHash) external view returns(Course memory) {
+         return ownedCourses[courseHash];
+     }
+     function getContractOwner() view public returns(address) {
+         return owner;
+     }
+     function setContractOwner(address newOwner) private {
+         owner = payable(newOwner);
+     }
+     function hasCourseOwnership(bytes32 courseHash) private view returns(bool){
+         return ownedCourses[courseHash].owner == msg.sender;
+     }
+}
+
+//course id = 10 ni Ascii text
          // 0x00000000000000000000000000003130 - couseId in hexaformad
          // 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 //40bytes 
          //000000000000000000000000000031305B38Da6a701c568545dCfcB03FcB875f56beddC4 
          //-> //0xc4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
          //     0xc4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
-         bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
-         return courseHash;
-     }
-}
